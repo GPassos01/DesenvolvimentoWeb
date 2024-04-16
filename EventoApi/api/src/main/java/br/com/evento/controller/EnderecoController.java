ef80b1da50190cd6fe755d7c94728b78ee945ba5
Your Name <you@example.com>
2024-04-15 21:41:12 -0300

    sla

--- EventoApi/api/src/main/java/br/com/evento/controller/EnderecoController.java
+++ EventoApi/api/src/main/java/br/com/evento/controller/EnderecoController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,13 @@ public class EnderecoController {
     @Autowired
     private EnderecoRepository enderecoRepository;
 
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Endereco> cadastrar(@RequestBody Endereco endereco){
+        Endereco enderecoSalvo = enderecoRepository.save(endereco);
+
+        return new ResponseEntity(enderecoSalvo, HttpStatus.OK);
+    }
+
     @GetMapping(value = "/", produces = "application/json")
     public ResponseEntity<List<Endereco>> getAll(){
         try{
@@ -32,4 +42,19 @@ public class EnderecoController {
             return new ResponseEntity("No such endereco", HttpStatus.NOT_FOUND);
         }
     }
+
+    @PutMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Endereco> atualizar (@RequestBody Endereco endereco){
+        Endereco enderecoSalvo = enderecoRepository.save(endereco);
+
+        return new ResponseEntity<Endereco>(enderecoSalvo, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = "application/json")
+    public String apagar(@PathVariable(value = "id")Long id){
+        enderecoRepository.deleteById(id);
+        
+        return "ok";
+    }
+
 }
