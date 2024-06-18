bd1d03b71b5f99979b02f2cae2a29f36bbeea69b
GPassos01 <gabriel.passos@unesp.br>
2024-06-18 18:52:24 -0300

    Função Put evento funcionando

--- EventoApi/api/src/main/java/br/com/evento/model/Evento.java
+++ EventoApi/api/src/main/java/br/com/evento/model/Evento.java
@@ -21,6 +21,12 @@ public class Evento {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     private String nome;
     private String descricao;
     private LocalDate data;
